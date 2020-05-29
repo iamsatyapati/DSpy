874f20dd4d5ab11020aedd210e5359e2cd506865
Satya Pati <iamsatyapati@gmail.com>
2020-05-29 19:32:25 +0530

    scrapper added

---
+++ DSpy/spiders/quote_scrapper.py
@@ -0,0 +1,20 @@
+# Spider to scrape quotes.toscrape.com
+
+import scrapy
+
+class QuoteScapper(scrapy.Spider):
+
+	name = "quotes"
+	start_urls = ['http://quotes.toscrape.com/']
+
+	def parse(self,response):
+
+		all_div_quotes = response.css("div.quote")
+		for quote in all_div_quotes:
+			title = quote.css("span.text::text").extract()
+			author = quote.css(".author::text").extract()
+			tag = quote.css(".tag::text").extract()
+
+			yield {
+			'title': title, 'author':author, 'tags': tag
+			}
